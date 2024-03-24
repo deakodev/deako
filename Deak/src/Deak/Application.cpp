1efff0446218226ee08cd33fd772529d509a9b8e
Zachary Deak <deak1zm@gmail.com>
2024-03-24 18:12:53 -0400

    Abstract away vertex buffer layouts

--- Deak/src/Deak/Application.cpp
+++ Deak/src/Deak/Application.cpp
@@ -12,6 +12,27 @@ namespace Deak
 
     Application* Application::s_Instance = nullptr;
 
+    static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+    {
+        switch (type)
+        {
+        case ShaderDataType::Float:    return GL_FLOAT;
+        case ShaderDataType::Float2:   return GL_FLOAT;
+        case ShaderDataType::Float3:   return GL_FLOAT;
+        case ShaderDataType::Float4:   return GL_FLOAT;
+        case ShaderDataType::Mat3:     return GL_FLOAT;
+        case ShaderDataType::Mat4:     return GL_FLOAT;
+        case ShaderDataType::Int:      return GL_INT;
+        case ShaderDataType::Int2:     return GL_INT;
+        case ShaderDataType::Int3:     return GL_INT;
+        case ShaderDataType::Int4:     return GL_INT;
+        case ShaderDataType::Bool:     return GL_BOOL;
+        }
+
+        DK_CORE_ASSERT(false, "Unknown ShaderDataType!");
+        return 0;
+    }
+
     Application::Application()
     {
         DK_CORE_ASSERT(!s_Instance, "Application already exists!");
@@ -26,16 +47,38 @@ namespace Deak
         glGenVertexArrays(1, &m_VertexArray);
         glBindVertexArray(m_VertexArray);
 
-        float vertices[3 * 3] = {
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            0.0f, 0.5f, 0.0f,
+        float vertices[3 * 7] = {
+            -0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+            0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+            0.0f, 0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
         };
 
         m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+        {
+            BufferLayout layout = {
+                { ShaderDataType::Float3, "a_Position" },
+                { ShaderDataType::Float4, "a_Color" },
+            };
+
+            m_VertexBuffer->SetLayout(layout);
+        }
+
+        uint32_t index = 0;
+        const auto& layout = m_VertexBuffer->GetLayout();
+        for (const auto& element : layout)
+        {
+            glEnableVertexAttribArray(index);
+            glVertexAttribPointer(
+                index,
+                element.GetComponentCount(),
+                ShaderDataTypeToOpenGLBaseType(element.Type),
+                element.Normalized ? GL_TRUE : GL_FALSE,
+                layout.GetStride(),
+                (const void*)(uintptr_t)element.Offset
+            );
+            index++;
+        }
 
         uint32_t indices[3] = { 0, 1, 2 };
         m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
@@ -44,21 +87,26 @@ namespace Deak
             #version 330 core
 
             layout(location = 0) in vec3 a_Position;
+            layout(location = 1) in vec4 a_Color;
+
+            out vec4 v_Color;
 
             void main()
             {
                 gl_Position = vec4(a_Position, 1.0);
+                v_Color = a_Color;
             }
         )";
 
         std::string fragmentSource = R"(
             #version 330 core
 
+            in vec4 v_Color;
             out vec4 fragColor;
 
             void main()
             {
-                fragColor = vec4(0.8, 0.2, 0.3, 1.0);
+                fragColor = vec4(v_Color);
             }
         )";
 
