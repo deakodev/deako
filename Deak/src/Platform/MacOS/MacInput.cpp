7cee5ed943183a0f4125fcc190ded82913c9b262
Zachary Deak <deak1zm@gmail.com>
2024-04-30 10:58:35 -0400

    Refactor input class to directly interact with platform specific cpp file

--- Deak/src/Platform/MacOS/MacInput.cpp
+++ Deak/src/Platform/MacOS/MacInput.cpp
@@ -1,25 +1,26 @@
+#include "Deak/Core/Input.h"
 #include "dkpch.h"
-#include "MacInput.h"
+
 #include "Deak/Core/Application.h"
 #include <GLFW/glfw3.h>
 
 namespace Deak {
 
-    bool MacInput::IsKeyPressedImpl(KeyCode keycode)
+    bool Input::IsKeyPressed(KeyCode keycode)
     {
         auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
         auto state = glfwGetKey(window, static_cast<int32_t>(keycode));
         return state == GLFW_PRESS || state == GLFW_REPEAT;
     }
 
-    bool MacInput::IsMouseButtonPressedImpl(MouseCode button)
+    bool Input::IsMouseButtonPressed(MouseCode button)
     {
         auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
         auto state = glfwGetMouseButton(window, static_cast<int32_t>(button));
         return state == GLFW_PRESS;
     }
 
-    std::pair<float, float> MacInput::GetMousePositionImpl()
+    std::pair<float, float> Input::GetMousePosition()
     {
         auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
         double xpos, ypos;
@@ -28,15 +29,15 @@ namespace Deak {
         return { (float)xpos, (float)ypos };
     }
 
-    float MacInput::GetMouseXImpl()
+    float Input::GetMouseX()
     {
-        auto [x, y] = GetMousePositionImpl();
+        auto [x, y] = GetMousePosition();
         return x;
     }
 
-    float MacInput::GetMouseYImpl()
+    float Input::GetMouseY()
     {
-        auto [x, y] = GetMousePositionImpl();
+        auto [x, y] = GetMousePosition();
         return y;
     }
 
